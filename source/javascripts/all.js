3b8939c6a1e15fb95effcd388e81974235f64021
Caden Lovelace <caden@herostrat.us>
2014-09-05 12:51:22 +0100

    Add internal link scrolling JS.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,21 @@
 //= require_tree .
 //= require jquery/dist/jquery.js
 //= require responsive-nav/responsive-nav.js
+
+$(function() {
+
+  $(document).on('click', '.internal-link', function(evt) {
+    var selector = '[name="'+evt.target.href.split('#')[1]+'"]';
+
+    if( $(selector).length !== 0 ) {
+      evt.preventDefault();
+
+      $('html, body').animate({
+        scrollTop: $(selector).offset().top
+      }, 200);
+    } else {
+      return true;
+    }
+  });
+
+});
