f53deb054939d683b9be5f1c3d4a3b3eb581613b
Caden Lovelace <caden@herostrat.us>
2014-10-03 13:22:27 +0100

    Make link scroller use pushState to adjust hash in URL as it would natively.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,16 +4,29 @@
 
 $(function() {
 
-  $(document).on('click', '.internal-link', function(evt) {
-    var selector = '[name="'+evt.target.href.split('#')[1]+'"]';
+  var navigateToHash = function(hash) {
+    var selector = '[name="'+hash+'"]';
 
     if( $(selector).length !== 0 ) {
-      evt.preventDefault();
-
       $('html, body').animate({
         scrollTop: $(selector).offset().top
       }, 200);
+
+      return true;
     } else {
+      return false;
+    }
+  };
+
+  $(document).on('click', '.internal-link', function(evt) {
+    try {
+      var hash =  evt.target.href.split('#')[1];
+      if( navigateToHash(hash) ) {
+        window.history.pushState({hash: hash}, '', '#'+hash);
+        evt.preventDefault();
+      }
+    } catch(e) {
+      // If there are any errors at all, save link functionality.
       return true;
     }
   });
