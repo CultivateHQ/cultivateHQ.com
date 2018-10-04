53a2db836a6af653601f970ca3134783ed141c50
spencer <hello@spencerturner.co.uk>
2018-10-04 15:59:19 +0100

    Refactored partials, got author links and pages working

--- config.rb
+++ config.rb
@@ -46,10 +46,18 @@ page 'index.html', layout: :home
 page '/posts/index.html', layout: :blog
 page '/posts/tag.html', layout: :blog
 page '/posts/calendar.html', layout: :blog
+page '/posts/author.html', layout: :author
 page '/jobs', layout: :job
 page '/newsletter-signup'
 page '/feed.xml', layout: false
 
+
+data.authors.collect {|author| author.keys.first }.each do |author_slug|
+  proxy "/posts/author/#{author_slug.gsub(/\s/,'-').downcase}",
+    '/posts/author.html',
+        locals: { author_slug: author_slug },
+        ignore: true
+end
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -64,6 +72,15 @@ page '/feed.xml', layout: false
 # Helpers
 ###
 
+# Methods defined in the helpers block are available in templates
+
+helpers do
+  def author_path(author)
+    @name = author.keys.first.gsub(/\s/,'-').downcase
+    "/posts/author/#{@name}"
+  end
+end
+
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
@@ -73,12 +90,6 @@ configure :development do
   config[:host] = 'http://localhost:4567'
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'stylesheets'
 
@@ -118,3 +129,5 @@ activate :directory_indexes
 
 # Activate the alias(redirect) plugin
 activate :alias
+
+
