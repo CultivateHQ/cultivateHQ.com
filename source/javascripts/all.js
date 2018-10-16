439abc6c562114b0a3603533aba9c03529dd502e
spencer <hello@spencerturner.co.uk>
2018-10-16 16:57:11 +0100

    Update menu size and add selected styling

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,7 +4,7 @@ $(function() {
   var slideout = new Slideout({
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
-    'padding': 256,
+    'padding': 200,
     'tolerance': 70,
     'side': 'right'
   })
