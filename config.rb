5904165d384401634d4792b7dae05c2e801f8558
spencer <hello@spencerturner.co.uk>
2019-02-21 10:42:50 +0000

    Add Sitemap plugin and configure.

--- config.rb
+++ config.rb
@@ -84,6 +84,9 @@ end
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
+# Sitemap Plugin
+activate :sitemap, :hostname => "https://cultivatehq.com"
+
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
